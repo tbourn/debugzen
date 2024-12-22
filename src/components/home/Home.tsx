72a38dfa0603ca44110f4f1bc44b07bd70c7f394
Thomas Bournaveas <t.bournaveas@protera.com>
2024-12-23 01:21:50 +0200

    Add base url.

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -10,9 +10,9 @@ const Home: React.FC = () => {
   const navigate = useNavigate();
 
   const images = [
-    '/images/carousel-1.png',
-    '/images/carousel-2.png',
-    '/images/carousel-3.png',
+    '/debugzen-frontend/images/carousel-1.png',
+    '/debugzen-frontend/images/carousel-2.png',
+    '/debugzen-frontend/images/carousel-3.png',
   ];
 
   return (
